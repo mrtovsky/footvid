8408d702c0092a4b634322b68fa417c0936f94b1
mrtovsky <mrtovsky@protonmail.com>
2020-09-22 18:32:18 +0200

    :sparkles: Add constant storing the final images size

--- footvid/utils/constants.py
+++ footvid/utils/constants.py
@@ -1,7 +1,9 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 
 REPOSITORY_NAME: str = "footvid"
+
+MODELING_SIZE: Tuple[int, int] = (224, 398)
 
 RGB_CHANNEL_STATS: Dict[str, List[float]] = {
     "mean": [0.485, 0.456, 0.406],
