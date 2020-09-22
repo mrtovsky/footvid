ba4c0effa3658acb827b436438b768a410b565b6
mrtovsky <mrtovsky@protonmail.com>
2020-09-22 18:35:17 +0200

    :sparkles: Adapt the ResNet50 model to the task
    Create a model inheriting pre-trained weights from ResNet50 trained on
    the ImageNet dataset, that is stipped from the last fully-connected
    layer replaced with the new linear layer adapted to binary as well as
    higher dimensional classification problems.
    
    Model is also storing a hook to gradients from the last convolutional
    layer of the original ResNet50 model, that will simplify Grad-CAM
    calculations.

---
+++ footvid/models.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class ResNet(nn.Module):
+    def __init__(self, output_size: int) -> None:
+        super().__init__()
+        resnet50 = models.resnet50(pretrained=True, progress=False)
+        conv_layers = []
+        for named_child in resnet50.named_children():
+            conv_layers.append(named_child)
+            if named_child[0] == "layer4":
+                break
+
+        self.resnet50_conv = nn.Sequential(OrderedDict(conv_layers))
+        self.avgpool = resnet50.avgpool
+        self.fc = nn.Linear(
+            in_features=resnet50.fc.in_features,
+            out_features=output_size,
+            bias=True,
+        )
+
+        self._gradients = None
+
+    @property
+    def gradients(self) -> torch.Tensor:
+        return self._gradients
+
+    def set_gradients(self, gradients: torch.Tensor) -> None:
+        self._gradients = gradients
+
+    def get_activations(self, x: torch.Tensor) -> torch.Tensor:
+        return self.resnet50_conv(x)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.resnet50_conv(x)
+        x.register_hook(self.set_gradients)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
