e21a2fe8bb7f96b3ba98bcbe34273acf59b092e2
mrtovsky <mrtovsky@protonmail.com>
2020-09-24 15:16:14 +0200

    :sparkles: Add function to reverse RGB channels normalization

--- footvid/preprocessing.py
+++ footvid/preprocessing.py
@@ -1,7 +1,13 @@
+from __future__ import annotations
+
+from typing import Optional, Sequence, TYPE_CHECKING
 import numpy as np
 from torchvision import transforms
 
 from footvid.utils.constants import MODELING_SIZE, RGB_CHANNEL_STATS
+
+if TYPE_CHECKING:
+    import torch
 
 
 TRAIN_TRANSFORMS = transforms.Compose(
@@ -31,3 +37,22 @@ TEST_TRANSFORMS = transforms.Compose(
         transforms.Normalize(**RGB_CHANNEL_STATS),
     ]
 )
+
+
+def denormalize(
+    x: torch.Tensor,
+    mean: Optional[Sequence[float]] = None,
+    std: Optional[Sequence[float]] = None,
+) -> np.ndarray:
+    if mean is None:
+        mean = RGB_CHANNEL_STATS["mean"]
+    if std is None:
+        std = RGB_CHANNEL_STATS["std"]
+
+    # Adjust to PIL Image format where RGB channel is the last dimension.
+    x = x.numpy().transpose(1, 2, 0)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    x = (x * std + mean).clip(0, 1)
+
+    return x
